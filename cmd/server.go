62e3f49fb9cbad2cc5ddce1a3908781f028e525a
pablo.vivo <pablo.vivo@integrationworks.co.th>
2020-08-11 10:13:06 +0700

    [add] geolocation example

--- cmd/server.go
+++ cmd/server.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"io/ioutil"
 	"log"
+	"math"
 	"net"
 	"os"
 	"strconv"
@@ -15,25 +16,41 @@ import (
 )
 
 var memory []uint16
+var geoMap map[string][]float64
+var GTruck GroupTrucks
 
+type GroupTrucks struct {
+	Trucks map[string]*Truck
+}
+type Truck struct {
+	Max  int
+	Next int
+	GPS  []float64
+}
+
+//var
 func main() {
 	port := flag.String("p", "40502", "Port to deploy Modbus")
 	mem := flag.String("mem", "", "Path to the configuration memory json")
-	mode := flag.Int("m", 3, "Mode of the server:	1 = ReadCoils, 2 = ReadDiscreteInputs, 3 = ReadHoldingRegisters, 4 = ReadInputRegisters, 5 = WriteSingleCoil, 6 = WriteHoldingRegister,	15 = WriteMultipleCoils, 16 = WriteHoldingRegisters ")
+	//mode := flag.Int("m", 3, "Mode of the server:	1 = ReadCoils, 2 = ReadDiscreteInputs, 3 = ReadHoldingRegisters, 4 = ReadInputRegisters, 5 = WriteSingleCoil, 6 = WriteHoldingRegister,	15 = WriteMultipleCoils, 16 = WriteHoldingRegisters ")
 	tick := flag.Int("t", 0, "Millisecond to trigger ontimer")
+	GTruck.Trucks = make(map[string]*Truck)
 
 	flag.Parse()
 	if *mem != "" {
-		memory = loadMemory(*mem)
+		//memory =  //loadMemory(*mem)
+		memory = make([]uint16, ^uint16(0))
+		geoMap = loadGeo(*mem)
+		for element := range geoMap {
+			GTruck.Trucks[element] = &Truck{Max: len(geoMap[element]), GPS: geoMap[element]}
+		}
 	}
+	GTruck.updateGeo()
 	serv := md.NewServer()
 	serv.HoldingRegisters = memory
-	if *mode != 0 {
-		serv.RegisterFunctionHandler(uint8(*mode), CustomHandler)
-	}
 	serv.OnConnectionHandler(ConnectionHandler)
 	if *tick != 0 {
-		serv.OnTimerHandler(TimerHandler, time.Duration(*tick)*time.Second)
+		serv.OnTimerHandler(TimerHandler, time.Duration(*tick)*time.Millisecond)
 	}
 
 	err := serv.ListenTCP("0.0.0.0:" + *port)
@@ -41,32 +58,13 @@ func main() {
 		log.Printf("%v\n", err)
 	}
 	defer serv.Close()
+
 	log.Println("[Author: kowiste] Modbus Server Active on port", *port)
 
 	// Wait forever
 	for {
 		time.Sleep(1 * time.Second)
 	}
-}
-
-//CustomHandler fucntion to customize the server response
-func CustomHandler(s *md.Server, frame md.Framer) ([]byte, *md.Exception) {
-	reg, numRegs, _ := frame.RegisterAddressAndNumber(frame)
-	data := make([]byte, numRegs+1)
-	data[0] = byte(numRegs) //the number of byte to send
-	dataPointer := 1        //Pointer of the first valid elemet in the array
-	if len(memory) >= reg+(numRegs/2) {
-		for n := 0; n < numRegs/2; n++ {
-			num := int16ToByte(memory[reg+n])
-			data[dataPointer] = num[0]
-			data[dataPointer+1] = num[1]
-			dataPointer += 2
-		}
-		log.Println("Reading Address: " + strconv.Itoa(reg) + " reading " + strconv.Itoa(numRegs) + " bytes")
-		return data, &md.Success
-	}
-	log.Println("Illegal Address")
-	return data, &md.IllegalDataAddress //return illegal addresss
 }
 
 //ConnectionHandler On connection
@@ -76,8 +74,8 @@ func ConnectionHandler(IP net.Addr) {
 
 //TimerHandler on timer handler pout the code you want to execute every time given
 func TimerHandler(s *md.Server) {
-	log.Println("Updating values")
-	s.HoldingRegisters[2]++
+	//log.Println("Updating values")
+	//GTruck.updateGeo()
 }
 
 func loadMemory(path string) []uint16 {
@@ -112,4 +110,48 @@ func int16ToByte(input uint16) []byte {
 	b := make([]byte, 2)
 	binary.BigEndian.PutUint16(b, input)
 	return b
+}
+func loadGeo(path string) map[string][]float64 {
+	mem := make(map[string][]float64)
+	b, err := ReadFile(path)
+	if err == nil {
+		err = json.Unmarshal(b, &mem)
+		if err != nil {
+			println(err.Error())
+			return nil
+		}
+	}
+
+	return mem
+}
+
+func doubleToByte(input []float64, pointer int) []uint16 {
+	buf := make([]uint16, 0)
+	n := math.Float64bits(input[pointer])
+	buf = append(buf, uint16(n>>48))
+	buf = append(buf, uint16(n>>32))
+	buf = append(buf, uint16(n>>16))
+	buf = append(buf, uint16(n))
+	n = math.Float64bits(input[pointer+1])
+	buf = append(buf, uint16(n>>48))
+	buf = append(buf, uint16(n>>32))
+	buf = append(buf, uint16(n>>16))
+	buf = append(buf, uint16(n))
+	return buf
+}
+
+func (t *GroupTrucks) updateGeo() {
+	for truck := range t.Trucks {
+		retMem := doubleToByte(geoMap[truck], t.Trucks[truck].Next) //Get the next 2 float for every truck
+		if t.Trucks[truck].Next < t.Trucks[truck].Max-2 {           //Check for the limit
+			t.Trucks[truck].Next += 2 //Pointer to the next element 2 position
+		} else {
+			t.Trucks[truck].Next = 0
+		}
+
+		num, _ := strconv.Atoi(truck)
+		for element := range retMem {
+			memory[num+element] = retMem[element]
+		}
+	}
 }
